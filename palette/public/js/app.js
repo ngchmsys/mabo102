9784f0b889ee975dd8f2b3e00c32a296a06721b1
ngchmsys <ngchmsys@gmail.com>
2021-01-22 12:07:37 +0900

    Update:renew

--- palette/public/js/app.js
+++ palette/public/js/app.js
@@ -1,6 +1,6 @@
-var PALETTE = `
+const PALETTE = `
 <div>
-    <p class="palette-color">{{ color.style_type }}({{color.setColor.r}}, {{color.setColor.g}}, {{color.setColor.b}}, {{color.setColor.a}})</p>
+    <p class="palette-color">{{ color.style_type }}({{color.setColor.r}}, {{color.setColor.g}}, {{color.setColor.b}})</p>
     <div class="palette-area" :style="{background: 'rgba(' + color.setColor.r + ',' + color.setColor.g + ',' + color.setColor.b + ',' + color.setColor.a + ')'}"></div>
 </div>
 `
@@ -19,12 +19,33 @@ var vm = new Vue({
             b: 0,
             a: 1.0
         },
-        style_type: "hex",
-        site_title: 'Palette'
+        style_type: "RGB",
+        site_title: 'Palette',
+        stockColors: [],
+        stock_limit: 9
     },
     methods: {
         stock: function(event) {
-            alert(event+" Stocked.")
+            console.dir(event)
+            console.log(this.setColor.r, this.setColor.g, this.setColor.b, this.setColor.a)
+            if(this.isLimit()) { 
+                this.stockColors.shift()
+            }
+            this.stockColors.push(this.setColor)
+            console.log('StockCount: ' + this.stockColors.length)
+            console.dir(this.stockColors)
+        },
+        isLimit: function() {
+            return (this.stockColors.length >= this.stock_limit)
+        },
+        reset: function(event) {
+            console.dir(event)
+            this.setColor.r = 0
+            this.setColor.g = 0
+            this.setColor.b = 0
+            this.setColor.a = 1.0
+            this.stockColors = []
+            console.log('StockCount: ' + this.stockColors.length)
         }
     }
 });
